7c6f7a11eaee5e5f755130d3963fa90ddf683528
itsaFan <steffansim@gmail.com>
2023-01-29 08:45:58 +0800

    upload files

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+          $('nav').addClass('black');
+    }
+
+    else {
+          $('nav').removeClass('black');
+    }
+})
+
+AOS.init();
